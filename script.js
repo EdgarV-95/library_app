72e3c8ae59058192b04b8c82bb7471390fb2527c
Edgar Veress <veress.edgar@yahoo.com>
2022-09-13 14:17:00 +0100

    Added delete and toggle button

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let myLibrary = []
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
+    this.read = read
 }
 
-const addBookToLibrary = (title, author, pages) => {
-    const book = new Book(title, author, pages)
+const addBookToLibrary = (title, author, pages, read) => {
+    const book = new Book(title, author, pages, read)
     myLibrary.push(book)
 }
 
@@ -21,10 +22,15 @@ const displayBook = () => {
     newDiv.innerHTML = 
     `<p class="title">Title: ${lastItem.title}</p>
     <p class="author">Author: ${lastItem.author}</p>
-    <p class="pages">Pages: ${lastItem.pages}</p>`
+    <p class="pages">Pages: ${lastItem.pages}</p>
+    <p class="read">Read: ${lastItem.read}</p>
+    <button class="del">Delete</button>
+    <button class="status">Toggle</button>
+    `
     document.getElementsByTagName("main")[0].appendChild(newDiv)
 }
 
+// Function to ADD new book
 const addNewBook = () => {
     // Create a form
     let form = document.createElement('form')
@@ -44,6 +50,12 @@ const addNewBook = () => {
         <label for="pages">Page number: </label>
         <input type="number" name="pages" id="pages">
     </div>
+    <div>
+        <label for="read">Read: </label>
+        <input type="checkbox" value="true" name="read" id="read" class="read">
+        <label for="not-read">Not read: </label>
+        <input type="checkbox" value="false" name="read" id="not-read" class="read">
+    </div>
     <button type="button" form="form1" value="Submit" id="submit-me">
         Submit
     </button>
@@ -54,19 +66,19 @@ const addNewBook = () => {
     // Run saveForm() once submit is clicked
     document.querySelector('#submit-me').addEventListener('click', saveForm)
 }
-// Add new book
 const addNewBtn = document.querySelector('.add-new')
 addNewBtn.addEventListener('click', addNewBook)
 
-// Saves the values in the form and adds them to addBookToLibrary() where a new class is created then printed out
+// Function to create a dynamic form and save it's values
 const saveForm = () => {
     // Get the value of each form input and save it into a variable
     titleValue = document.querySelector('#title').value
     authorValue = document.querySelector('#author').value
     pagesValue = document.querySelector('#pages').value
+    readValue = document.querySelector('.read:checked').value
 
     // Create a new Book object with the values
-    addBookToLibrary(titleValue, authorValue, pagesValue)
+    addBookToLibrary(titleValue, authorValue, pagesValue, readValue)
 
     // Remove form from html after submit btn has been clicked
     document.querySelector('.create-form').remove();
